43bccf719bd1cf226df34413ad81599d20d74d12
Lootgvfr <brdmonster@gmail.com>
2020-02-08 16:01:17 +0200

    add base classes

---
+++ src/game/base/object.js
@@ -0,0 +1,21 @@
+import { Entity } from "./entity";
+
+class ObjectEntity extends Entity {
+    constructor(
+        {
+            display_data = {},
+            pos_x = 0,
+            pos_y = 0
+        }
+    ) {
+        super(
+            {
+                display_data: display_data,
+                pos_x: pos_x,
+                pos_y: pos_y
+            }
+        );
+    }
+}
+
+export { ObjectEntity };
